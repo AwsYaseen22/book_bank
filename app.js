2e101ec7be36b7e1575d07a9fc9b5d9d7bc6e01b
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-08-04 14:48:22 +0300

    connected to the db

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
 // attach the config file with dotenv
 dotenv.config({ path: './config/config.env' })
 
+connectDB()
 const app = express()
 
 const PORT = process.env.PORT || 3000
