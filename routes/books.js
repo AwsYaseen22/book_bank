51bd377c316f69d3eb951fad6937164831220cfd
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-08-08 00:38:57 +0300

    almost all functionality done need the enhancement in the frontend

--- routes/books.js
+++ routes/books.js
@@ -27,7 +27,47 @@ router.get("/", async (request, response) => {
     console.error({ error });
     return response.render("pages/errors/500");
   }
-  response.render("pages/books/add.ejs");
+});
+
+// show selected book details
+router.get("/:id", ensureAuth, async (request, response) => {
+  try {
+    const book = await Book.findById(request.params.id)
+      .populate("user") // get the full detail of the user belongs to this book
+      .lean(); // return pure object
+    if (!book) {
+      return response.render("pages/errors/404");
+    }
+    return response.render("pages/books/book-details", {
+      book,
+      formatDate,
+      image: book.user.image,
+    });
+  } catch (error) {
+    console.error({ error });
+    return response.render("pages/errors/500");
+  }
+});
+
+// show books read by specific user
+router.get("/user/:id", async (request, response) => {
+  try {
+    const allBooks = await Book.find({
+      user: request.params.id,
+      status: "public",
+    })
+      .populate("user") // get the full detail of the user belongs to this book
+      .sort({ createdAt: "desc" })
+      .lean(); // return pure object
+    return response.render("pages/books/public-books", {
+      allBooks,
+      formatDate,
+      truncate,
+    });
+  } catch (error) {
+    console.error({ error });
+    return response.render("pages/errors/500");
+  }
 });
 
 // save new book to the db
@@ -46,51 +86,66 @@ router.post("/", ensureAuth, async (request, response) => {
 
 // show EDIT book page
 router.get("/edit/:id", ensureAuth, async (request, response) => {
-  const book = await Book.findById(request.params.id).lean();
+  try {
+    const book = await Book.findById(request.params.id).lean();
 
-  if (!book) {
-    return response.render("pages/errors/404");
-  }
-  // ensure the user logged in is the same as the book user
-  if (book.user.toJSON() !== request.user.id) {
-    response.redirect("/");
-  } else {
-    response.render("pages/books/edit.ejs", { book });
+    if (!book) {
+      return response.render("pages/errors/404");
+    }
+    // ensure the user logged in is the same as the book user
+    if (book.user.toJSON() !== request.user.id) {
+      response.redirect("/");
+    } else {
+      response.render("pages/books/edit.ejs", { book });
+    }
+  } catch (error) {
+    console.error(error);
+    response.render("pages/errors/500");
   }
 });
 
 // edit the book on the db
 router.put("/:id", ensureAuth, async (request, response) => {
-  const book = await Book.findById(request.params.id).lean();
+  try {
+    const book = await Book.findById(request.params.id).lean();
 
-  if (!book) {
-    return response.render("pages/errors/404");
-  }
-  // ensure the user logged in is the same as the book user
-  if (book.user.toJSON() !== request.user.id) {
-    response.redirect("/");
-  } else {
-    await Book.findByIdAndUpdate({ _id: request.params.id }, request.body, {
-      new: true,
-      runValidators: true,
-    });
-    response.redirect("/dashboard");
+    if (!book) {
+      return response.render("pages/errors/404");
+    }
+    // ensure the user logged in is the same as the book user
+    if (book.user.toJSON() !== request.user.id) {
+      response.redirect("/");
+    } else {
+      await Book.findByIdAndUpdate({ _id: request.params.id }, request.body, {
+        new: true,
+        runValidators: true,
+      });
+      response.redirect("/dashboard");
+    }
+  } catch (error) {
+    console.error(error);
+    response.render("pages/errors/500");
   }
 });
 
 // delete a book from the db
 router.delete("/:id", ensureAuth, async (request, response) => {
-  const book = await Book.findById(request.params.id).lean();
+  try {
+    const book = await Book.findById(request.params.id).lean();
 
-  if (!book) {
-    return response.render("pages/errors/404");
-  }
-  // ensure the user logged in is the same as the book user
-  if (book.user.toJSON() !== request.user.id) {
-    response.redirect("/");
-  } else {
-    await Book.findByIdAndDelete({ _id: request.params.id });
-    response.redirect("/dashboard");
+    if (!book) {
+      return response.render("pages/errors/404");
+    }
+    // ensure the user logged in is the same as the book user
+    if (book.user.toJSON() !== request.user.id) {
+      response.redirect("/");
+    } else {
+      await Book.findByIdAndDelete({ _id: request.params.id });
+      response.redirect("/dashboard");
+    }
+  } catch (error) {
+    console.error(error);
+    response.render("pages/errors/500");
   }
 });
 
